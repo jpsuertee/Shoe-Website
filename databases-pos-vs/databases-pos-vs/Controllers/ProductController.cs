703a2c817f9ae761e4dca81e2cfdefd41afa0852
chichen <oscers1001dennis@gmail.com>
2022-03-30 20:57:58 -0500

    Created ProductController and corresponding View files and Model

---
+++ databases-pos-vs/databases-pos-vs/Controllers/ProductController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using databases_pos_vs.Data;
+using databseApp.Models;
+
+namespace databases_pos_vs.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly databases_pos_vsContext _context;
+
+        public ProductController(databases_pos_vsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Product
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ProductViewModel.ToListAsync());
+        }
+
+        // GET: Product/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productViewModel = await _context.ProductViewModel
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (productViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(productViewModel);
+        }
+
+        // GET: Product/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,Size,Price,Name,Category_id,Vendor_id")] ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productViewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productViewModel);
+        }
+
+        // GET: Product/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productViewModel = await _context.ProductViewModel.FindAsync(id);
+            if (productViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(productViewModel);
+        }
+
+        // POST: Product/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Size,Price,Name,Category_id,Vendor_id")] ProductViewModel productViewModel)
+        {
+            if (id != productViewModel.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductViewModelExists(productViewModel.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productViewModel);
+        }
+
+        // GET: Product/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productViewModel = await _context.ProductViewModel
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (productViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(productViewModel);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productViewModel = await _context.ProductViewModel.FindAsync(id);
+            _context.ProductViewModel.Remove(productViewModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductViewModelExists(int id)
+        {
+            return _context.ProductViewModel.Any(e => e.ProductId == id);
+        }
+    }
+}
---
+++ databases-pos-vs/databases-pos-vs/Models/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace databseApp.Models
+{
+    public class ProductViewModel
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Size { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Should be greated than or equal to 1")]
+        public float Price { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public int Category_id { get; set; }
+        public int Vendor_id { get; set; }
+    }
+}
