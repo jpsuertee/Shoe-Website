5a91c1e967a9386522398f4d26a5392414c69ca7
Jason <jasonsuerte23@gmail.com>
2022-03-31 13:59:38 -0500

    Added user files

---
+++ databases-pos-vs/databases-pos-vs/Controllers/UserController.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using databseApp.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace databseApp.Controllers
+{
+    public class UserController : Controller
+    {
+        //private readonly databseAppContext _context;
+
+        public bool logged_in = false;
+        //NOTE: this variable will be used to determined if logged in or not
+
+
+        public UserController(/*databseAppContext context*/)
+        {
+            
+        }
+
+        // GET: User
+        public IActionResult UserIndex()
+        {
+            return View();
+            //function to go to account settings/reports
+        }
+
+        // GET: User/Create/?
+        public IActionResult Create()
+        {
+            UserViewModel userviewmodel = new UserViewModel();
+            return View();
+        }
+
+        /*
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userViewModel = await _context.UserViewModel
+                .FirstOrDefaultAsync(m => m.UserID == id);
+            if (userViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(userViewModel);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserID,password")] UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userViewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userViewModel);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userViewModel = await _context.UserViewModel.FindAsync(id);
+            if (userViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(userViewModel);
+        }
+        
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserID,password")] UserViewModel userViewModel)
+        {
+            if (id != userViewModel.UserID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserViewModelExists(userViewModel.UserID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userViewModel);
+        }
+        
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userViewModel = await _context.UserViewModel
+                .FirstOrDefaultAsync(m => m.UserID == id);
+            if (userViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(userViewModel);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var userViewModel = await _context.UserViewModel.FindAsync(id);
+            _context.UserViewModel.Remove(userViewModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        
+        private bool UserViewModelExists(int id)
+        {
+            return _context.UserViewModel.Any(e => e.UserID == id);
+        }
+        */
+    }
+}
