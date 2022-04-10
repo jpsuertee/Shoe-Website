0e35c0295995e6dd297582cd9da31ced8a8688dc
Liam Amadio <liam@m1pro.local>
2022-04-10 13:30:40 -0500

    add to cart feature

--- databases-pos-vs/databases-pos-vs/Controllers/HomeController.cs
+++ databases-pos-vs/databases-pos-vs/Controllers/HomeController.cs
@@ -47,9 +47,43 @@ namespace databases_pos_vs.Controllers
             return View();
         }
 
+        public IActionResult AddToCart(int id)
+        {
+            string idString = id.ToString();
+
+            if (!HttpContext.Request.Cookies.ContainsKey("CartCookie"))
+                HttpContext.Response.Cookies.Append("CartCookie", idString);
+
+            else
+            {
+                string newCart = HttpContext.Request.Cookies["CartCookie"] + "," + idString;
+                HttpContext.Response.Cookies.Append("CartCookie", newCart);
+            }
+
+
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
         public IActionResult Cart()
         {
-            return View();
+
+            MySqlDataAdapter daProducts;
+            DataTable dtbl = new DataTable();
+
+            using (MySqlConnection sqlConnection = new MySqlConnection(_configuration.GetConnectionString("DevConnection")))
+            {
+                sqlConnection.Open();
+                string cartString = HttpContext.Request.Cookies["CartCookie"];
+                string sql = String.Format("SELECT * FROM Products WHERE Products.product_id IN ({0})", cartString);
+                daProducts = new MySqlDataAdapter(sql, sqlConnection);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(daProducts);
+                daProducts.Fill(dtbl);
+
+            }
+            return View(dtbl);
+   
         }
 
     }
